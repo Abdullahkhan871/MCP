d718ade2eb73d740bcfeba914c342d91a1ecfe6a
Abdullahkhan871 <akhan283785@gmail.com>
2025-09-01 23:56:24 +0530

    Refactor index.js to use ES module syntax and enhance chat functionality; update package.json and package-lock.json for new dependencies

--- client/index.js
+++ client/index.js
@@ -1,14 +1,11 @@
-const readline = require("readline/promises");
-const { GoogleGenAI } = require("@google/genai");
+import { config } from "dotenv";
+import readline from "readline/promises";
+import { GoogleGenAI } from "@google/genai"
 
-const history = [];
+config();
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdin,
-})
+const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GEMINI_API_KEY });
 
-const ai = new GoogleGenAI({ apiKey: "YOUR_API_KEY" });
 async function main() {
     const response = await ai.models.generateContent({
         model: "gemini-2.0-flash",
@@ -16,9 +13,43 @@ async function main() {
     });
     console.log(response.text);
 }
+await main();
 
-main();
+const chatHistory = [];
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
 
 async function chatLoop(params) {
     const question = await rl.question("You: ")
+
+    chatHistory.push({
+        role: "user",
+        parts: [
+            {
+                text: question,
+                type: "text",
+            }
+        ]
+    });
+    const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: chatHistory,
+    });
+
+    chatHistory.push({
+        role: "model",
+        parts: [
+            {
+                text: response.candidates[0].content.parts[0].text,
+                type: "text",
+            }
+        ]
+    })
+    console.log(`AI : ${response.candidates[0].content.parts[0].text}`)
+    chatLoop();
+
 }
+chatLoop();
